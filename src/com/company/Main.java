dd7018e7725ed15514f5b08954dcf530034edabb
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-09-24 21:04:52 +0400

    i popoxakan fori mej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,30 +13,27 @@ public class Main {
         }
         System.out.println("n = " + n++);
 
-
         // tpel kent tvery
-        int k;
         System.out.println("0-100 mijakayqum kent tvern en");
-        for (k = 0; k < 100; k++) {
-            if (k % 2 != 0) {
-                System.out.println("k = " + k);
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("i = " + i);
             }
         }
         // tpel 3-i bajanvox tvery
-        int m;
         System.out.println("0 -100 mijakayqum tver voronq bajanvum en 3-i");
-        for (m = 1; m <= 100; m++) {
-            if (m % 3 == 0) {
-                System.out.println("m = " + m);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("i = " + i);
             }
         }
-        // tpel 0-100 tveri actorialy
-        int i;
-        double j = 1;
+
+        // tpel 0-100 tveri factorialy
+        double y = 1;
         System.out.println("0-100 tveri faktorial");
-        for (i = 1; i <= 100; i++) {
-            j = j * i;
-            System.out.println("j = " + j);
+        for (int i = 1; i <= 100; i++) {
+            y = y * i;
+            System.out.println("l = " + y);
         }
 
         // tpel 0-100 tveri gumary
@@ -49,78 +46,77 @@ public class Main {
         System.out.println("l = " + l);
 
         // tpel patkerner
-
-        for (int a = 1; a < 10; a++) {
-            for (int b = a; b < 10; b++) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = i; j < 10; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for (int yy = 1; yy < 10; yy++) {
-            for (int jj = 1; jj < yy; jj++) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < i; j++) {
                 System.out.print("-");
 
             }
             System.out.println();
         }
 
-        for (int a = 1; a < 7; a++) {
-            for (int b = 7; b >= a; b--) {
+        for (int i = 1; i < 7; i++) {
+            for (int j = 7; j >= i; j--) {
                 System.out.print(" ");
             }
-            for (int c = a; c > 0; c--) {
+            for (int c = i; c > 0; c--) {
                 System.out.print(" " + c);
             }
             System.out.println(" ");
         }
 
         // fory fori mej
-        for (int p = 1; p < 11; p++) {
-            System.out.println("p = " + p);
+        for (int i = 1; i < 11; i++) {
+            System.out.println("i = " + i);
             System.out.println("^^^^^^^^^^^^^^^");
 
-            for (int y = 1; y < 11; y++) {
-                System.out.println(" y = " + y);
+            for (int j = 1; j < 11; j++) {
+                System.out.println(" j = " + j);
             }
             System.out.println("^^^^^^^^^^^^^^^");
         }
         // whiley whilei mej
-        int p = 1;
-        while (p < 11) {
-            System.out.println("p = " + p);
-            System.out.println("&&&&&&&&&&&&&&&&");
-            int y = 1;
-            while (y < 11) {
-                System.out.println("y = " + y);
-                y++;
+        int i = 1;
+        while (i < 11) {
+            System.out.println("i = " + i);
+            System.out.println("///////////");
+            int j = 1;
+            while (j < 11) {
+                System.out.println("j = " + j);
+                j++;
             }
-            p++;
+            i++;
         }
-        System.out.println("&&&&&&&&&&&&&&&&");
+        System.out.println("///////////");
 
         // fory whili mej
-        int uu = 1;
-        while (uu < 11) {
-            System.out.println("uu = " + uu);
-            System.out.println("@@@@@@@@@@@@@@");
-            for (int tt = 1; tt < 11; tt++) {
-                System.out.println("tt = " + tt);
+        int a = 1;
+        while (a < 11) {
+            System.out.println("a = " + a);
+            System.out.println("_____________");
+            for (int b = 1; b < 11; b++) {
+                System.out.println("b = " + b);
             }
-            uu++;
+            a++;
         }
-        System.out.println("@@@@@@@@@@@@@@@@");
+        System.out.println("_____________");
 
         // whily fori mej
         for (int d = 1; d < 11; d++) {
             System.out.println("d = " + d);
-            System.out.println("!!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!");
             int s = 1;
             while (s < 11) {
                 System.out.println("s = " + s);
                 s++;
             }
-            System.out.println("!!!!!!!!!!!!!!!!!");
+            System.out.println("!!!!!!!!!!!!!!!");
         }
     }
 }
