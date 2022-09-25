9cca5d70b7ab48838923832dc1b6d3f0858b97d1
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-09-25 20:18:16 +0400

    grqi orinaky

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.IOException;
 
 public class Main {
@@ -13,117 +11,117 @@ public class Main {
         }
         char choice;
         do {
-                System.out.println(" Cпpaвкa по оператору : ");
-                System.out.println(" 1. i f ");
-                System.out.println(" 2. switch");
-                System.out.println(" З. whil e");
-                System.out.println(" 4. do-while ");
-                System.out.println(" 5. for\n");
-                System.out.println(" Bыбepитe нужный пункт:");
-                choice = (char) System.in.read();
-            }
-            while (choice < '1' || choice > '5') ;
-            System.out.println("\n");
-            switch (choice) {
-                case '1':
-                    System.out.println("if: \n");
-                    System.out.println("if ( ycлoвиe) оператор;");
-                    System.out.println("else оператор;");
-                    break;
-                case '2':
-                    System.out.println(" switch:\n");
-                    System.out.println(" switch ( выpaжeниe ) {");
-                    System.out.println(" сазе константа : ");
-                    System.out.println(" последовательность операторов ");
-                    System.out.println(" break; ");
-                    System.out.println(" // ... ");
-                    System.out.println(" } ");
-                    break;
-                case '3':
-                    System.out.println("while : \n");
-                    System.out.println("while ( ycлoвиe ) оператор ; ");
-                    break;
-                case '4':
-                    System.out.println(" do-whi le : \n ");
-                    System.out.println("do {");
-                    System.out.println(" оператор ; ");
-                    System.out.println("} while ( условие ) ;");
-                    break;
-                case '5':
-                    System.out.println("for : \n");
-                    System.out.print("for ( инициaлиэaция; условие ; итерация } ");
-                    System.out.println(" оператор ; ");
-                    break;
-            }
+            System.out.println(" Cпpaвкa по оператору : ");
+            System.out.println(" 1. i f ");
+            System.out.println(" 2. switch");
+            System.out.println(" З. whil e");
+            System.out.println(" 4. do-while ");
+            System.out.println(" 5. for\n");
+            System.out.println(" Bыбepитe нужный пункт:");
+            choice = (char) System.in.read();
+        }
+        while (choice < '1' || choice > '5');
+        System.out.println("\n");
+        switch (choice) {
+            case '1':
+                System.out.println("if: \n");
+                System.out.println("if ( ycлoвиe) оператор;");
+                System.out.println("else оператор;");
+                break;
+            case '2':
+                System.out.println(" switch:\n");
+                System.out.println(" switch ( выpaжeниe ) {");
+                System.out.println(" сазе константа : ");
+                System.out.println(" последовательность операторов ");
+                System.out.println(" break; ");
+                System.out.println(" // ... ");
+                System.out.println(" } ");
+                break;
+            case '3':
+                System.out.println("while : \n");
+                System.out.println("while ( ycлoвиe ) оператор ; ");
+                break;
+            case '4':
+                System.out.println(" do-whi le : \n ");
+                System.out.println("do {");
+                System.out.println(" оператор ; ");
+                System.out.println("} while ( условие ) ;");
+                break;
+            case '5':
+                System.out.println("for : \n");
+                System.out.print("for ( инициaлиэaция; условие ; итерация } ");
+                System.out.println(" оператор ; ");
+                break;
+        }
 
-            // tpel zuyg tvery
-            int n;
-            System.out.println("0-100 mijakayqum zuyq tvern en");
-            for (n = 0; n < 100; n++) {
-                if (n % 2 == 0) {
-                    System.out.println("n = " + n);
-                }
+        // tpel zuyg tvery
+        int n;
+        System.out.println("0-100 mijakayqum zuyq tvern en");
+        for (n = 0; n < 100; n++) {
+            if (n % 2 == 0) {
+                System.out.println("n = " + n);
             }
-            System.out.println("n = " + n++);
+        }
+        System.out.println("n = " + n++);
 
-            // tpel kent tvery
-            System.out.println("0-100 mijakayqum kent tvern en");
-            for (int i = 0; i < 100; i++) {
-                if (i % 2 != 0) {
-                    System.out.println("i = " + i);
-                }
+        // tpel kent tvery
+        System.out.println("0-100 mijakayqum kent tvern en");
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("i = " + i);
             }
-            // tpel 3-i bajanvox tvery
-            System.out.println("0 -100 mijakayqum tver voronq bajanvum en 3-i");
-            for (int i = 1; i <= 100; i++) {
-                if (i % 3 == 0) {
-                    System.out.println("i = " + i);
-                }
+        }
+        // tpel 3-i bajanvox tvery
+        System.out.println("0 -100 mijakayqum tver voronq bajanvum en 3-i");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("i = " + i);
             }
+        }
 
-            // tpel 0-100 tveri factorialy
-            double y = 1;
-            System.out.println("0-100 tveri faktorial");
-            for (int i = 1; i <= 100; i++) {
-                y = y * i;
-                System.out.println("l = " + y);
+        // tpel 0-100 tveri factorialy
+        double y = 1;
+        System.out.println("0-100 tveri faktorial");
+        for (int i = 1; i <= 100; i++) {
+            y = y * i;
+            System.out.println("l = " + y);
+        }
+
+        // tpel 0-100 tveri gumary
+        int l = 0, h = 0;
+        System.out.println("0-100 tveri gumary");
+        while (h <= 100) {
+            l = l + h;
+            h++;
+        }
+        System.out.println("l = " + l);
+
+        // tpel patkerner
+
+        /*
+         ******
+         *****
+         ****
+         ***
+         **
+         *
+
+         */
+        for (int i = 1; i < 10; i++) {
+            for (int j = i; j < 10; j++) {
+                System.out.print("*");
             }
+            System.out.println();
+        }
 
-            // tpel 0-100 tveri gumary
-            int l = 0, h = 0;
-            System.out.println("0-100 tveri gumary");
-            while (h <= 100) {
-                l = l + h;
-                h++;
+        System.out.println("-------------------");
+
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("%");
             }
-            System.out.println("l = " + l);
-
-            // tpel patkerner
-
-            /*
-             ******
-             *****
-             ****
-             ***
-             **
-             *
-
-             */
-            for (int i = 1; i < 10; i++) {
-                for (int j = i; j < 10; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-
-            System.out.println("-------------------");
-
-            for (int i = 4; i >= 0; i--) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("%");
-                }
-                System.out.println();
-            }
+            System.out.println();
+        }
 
         /*
         -
@@ -132,14 +130,14 @@ public class Main {
         ----
         -----
          */
-            System.out.println("-------------------");
-            for (int i = 1; i < 10; i++) {
-                for (int j = 1; j < i; j++) {
-                    System.out.print("-");
+        System.out.println("-------------------");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("-");
 
-                }
-                System.out.println();
             }
+            System.out.println();
+        }
 
         /*
                1
@@ -147,16 +145,16 @@ public class Main {
              3 2 1
             4 3 2 1
          */
-            System.out.println("-------------------");
-            for (int i = 1; i < 7; i++) {
-                for (int j = 7; j >= i; j--) {
-                    System.out.print(" ");
-                }
-                for (int c = i; c > 0; c--) {
-                    System.out.print(" " + c);
-                }
-                System.out.println(" ");
+        System.out.println("-------------------");
+        for (int i = 1; i < 7; i++) {
+            for (int j = 7; j >= i; j--) {
+                System.out.print(" ");
             }
+            for (int c = i; c > 0; c--) {
+                System.out.print(" " + c);
+            }
+            System.out.println(" ");
+        }
         /*
         qarakusi
         * * * *
@@ -164,13 +162,13 @@ public class Main {
         * * * *
         * * * *
          */
-            System.out.println("-------------------");
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    System.out.print(" # ");
-                }
-                System.out.println();
+        System.out.println("-------------------");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" # ");
             }
+            System.out.println();
+        }
 
         /*
            # ## ### ####
@@ -178,17 +176,17 @@ public class Main {
            # ##
            #
          */
-            System.out.println("-------------------");
-            for (int i = 4; i >= 0; i--) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(" ");
-                    for (int k = 0; k < j; k++) {
-                        System.out.print("#");
-                    }
+        System.out.println("-------------------");
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+                for (int k = 0; k < j; k++) {
+                    System.out.print("#");
                 }
-
-                System.out.println();
             }
+
+            System.out.println();
+        }
 
         /*
             $
@@ -197,17 +195,17 @@ public class Main {
          $$$$
         $$$$$
          */
-            System.out.println("-------------------");
-            for (int i = 0; i <= 6; i++) {
-                for (int j = 6; j >= i; j--) {
-                    System.out.print(" ");
-                }
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("$");
-                }
-                System.out.println();
+        System.out.println("-------------------");
+        for (int i = 0; i <= 6; i++) {
+            for (int j = 6; j >= i; j--) {
+                System.out.print(" ");
             }
-            System.out.println("-------------------");
+            for (int j = 0; j <= i; j++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------");
         /*
         ######
          #####
@@ -216,64 +214,64 @@ public class Main {
             ##
              #
          */
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(" ");
-                }
-                for (int j = 6; j > i; j--) {
-                    System.out.print("#");
-                }
-                System.out.println();
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
             }
-            // fory fori mej
-            for (int i = 1; i < 11; i++) {
-                System.out.println("i = " + i);
-                System.out.println("^^^^^^^^^^^^^^^");
+            for (int j = 6; j > i; j--) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        // fory fori mej
+        for (int i = 1; i < 11; i++) {
+            System.out.println("i = " + i);
+            System.out.println("^^^^^^^^^^^^^^^");
 
-                for (int j = 1; j < 11; j++) {
-                    System.out.println(" j = " + j);
-                }
-                System.out.println("^^^^^^^^^^^^^^^");
+            for (int j = 1; j < 11; j++) {
+                System.out.println(" j = " + j);
             }
-            // whiley whilei mej
-            int i = 1;
-            while (i < 11) {
-                System.out.println("i = " + i);
-                System.out.println("///////////");
-                int j = 1;
-                while (j < 11) {
-                    System.out.println("j = " + j);
-                    j++;
-                }
-                i++;
-            }
+            System.out.println("^^^^^^^^^^^^^^^");
+        }
+        // whiley whilei mej
+        int i = 1;
+        while (i < 11) {
+            System.out.println("i = " + i);
             System.out.println("///////////");
-
-            // fory whili mej
-            int a = 1;
-            while (a < 11) {
-                System.out.println("a = " + a);
-                System.out.println("_____________");
-                for (int b = 1; b < 11; b++) {
-                    System.out.println("b = " + b);
-                }
-                a++;
+            int j = 1;
+            while (j < 11) {
+                System.out.println("j = " + j);
+                j++;
             }
+            i++;
+        }
+        System.out.println("///////////");
+
+        // fory whili mej
+        int a = 1;
+        while (a < 11) {
+            System.out.println("a = " + a);
             System.out.println("_____________");
-
-            // whily fori mej
-            for (int d = 1; d < 11; d++) {
-                System.out.println("d = " + d);
-                System.out.println("!!!!!!!!!!!!!!!");
-                int s = 1;
-                while (s < 11) {
-                    System.out.println("s = " + s);
-                    s++;
-                }
-                System.out.println("!!!!!!!!!!!!!!!");
+            for (int b = 1; b < 11; b++) {
+                System.out.println("b = " + b);
             }
+            a++;
+        }
+        System.out.println("_____________");
+
+        // whily fori mej
+        for (int d = 1; d < 11; d++) {
+            System.out.println("d = " + d);
+            System.out.println("!!!!!!!!!!!!!!!");
+            int s = 1;
+            while (s < 11) {
+                System.out.println("s = " + s);
+                s++;
+            }
+            System.out.println("!!!!!!!!!!!!!!!");
         }
     }
+}
 
 
 
